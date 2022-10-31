d42907c04dec8988af14d54f5b796c17c38a900a
Md Tariqul Islam <60572580+Md-Tariqul-Islam-BU@users.noreply.github.com>
2022-10-31 16:58:00 +0600

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "db_pro");
+define("TITLE", "Md. Tariqul Islam");
+define("KEYWORDS", "PHP Tutorials, JAVA Tutorials");
